322cb801b3766e702e7ed485bd3f7f509202ed36
Jeffrey Pfau <jeffrey@endrift.com>
2012-07-08 19:24:55 -0700

    Implement CpuSet

--- irq.js
+++ irq.js
@@ -8,6 +8,51 @@ GameBoyAdvanceInterruptHandler.prototype.setCPU = function(cpu) {
 
 GameBoyAdvanceInterruptHandler.prototype.swi = function(opcode) {
 	switch (opcode) {
+	case 0x0B:
+		// CpuSet
+		var source = this.cpu.gprs[0];
+		var dest = this.cpu.gprs[1];
+		var mode = this.cpu.gprs[2];
+		var count = mode & 0x000FFFFF;
+		var fill = mode & 0x01000000;
+		var wordsize = (mode & 0x04000000) ? 4 : 2;
+		if (fill) {
+			if (wordsize == 4) {
+				var word = this.cpu.mmu.load32(source);
+				for (var i = 0; i < count; ++i) {
+					this.cpu.mmu.store32(dest + (i << 2), word);
+				}
+			} else {
+				var word = this.cpu.mmu.load16(source);
+				for (var i = 0; i < count; ++i) {
+					this.cpu.mmu.store16(dest + (i << 1), word);
+				}
+			}
+		} else {
+			if (wordsize == 4) {
+				for (var i = 0; i < count; ++i) {
+					var word = this.cpu.mmu.load32(source + (i << 2));
+					this.cpu.mmu.store32(dest + (i << 2), word);
+				}
+			} else {
+				var i = 0;
+				var word;
+				if (source & 0x00000002) {
+					word = this.cpu.mmu.load16(source);
+					this.cpu.mmu.store16(dest, word);
+					++i;
+				}
+				for (; i + 1 < count; i += 2) {
+					word = this.cpu.mmu.load32(source + (i << 1));
+					this.cpu.mmu.store32(dest + (i << 1), word);
+				}
+				if (i < count) {
+					word = this.cpu.mmu.load16(source + (i << 1));
+					this.cpu.mmu.store16(dest + (i << 1), word);
+				}
+			}
+		}
+		return;
 	default:
 		throw "Unimplemented software interrupt: 0x" + opcode.toString(16);
 	}
