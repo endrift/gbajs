9f347f8e7760af1e4a3f028937cbfa36364c7e8f
Jeffrey Pfau <jeffrey@endrift.com>
2013-03-19 23:37:07 -0700

    Add fixed branches to the linked list

--- js/core.js
+++ js/core.js
@@ -106,7 +106,6 @@ ARMCore.prototype.resetCPU = function(startOffset) {
 				this.instruction = instruction.next;
 			}
 		} else {
-			this.instruction = null;
 			if (this.conditionPassed) {
 				var pc = gprs[this.PC] &= 0xFFFFFFFE;
 				if (this.execMode == this.MODE_ARM) {
@@ -117,6 +116,16 @@ ARMCore.prototype.resetCPU = function(startOffset) {
 					mmu.waitSeq(pc);
 				}
 				gprs[this.PC] += this.instructionWidth;
+				if (!instruction.fixedJump) {
+					this.instruction = null;
+				} else if  (this.instruction != null) {
+					if (instruction.next == null) {
+						instruction.next = this.loadInstruction(gprs[this.PC] - this.instructionWidth);
+					}
+					this.instruction = instruction.next;
+				}
+			} else {
+				this.instruction = null;
 			}
 		}
 		++this.cycles;
@@ -299,6 +308,7 @@ ARMCore.prototype.badOp = function(instruction) {
 		throw "Illegal instruction: 0x" + instruction.toString(16);
 	};
 	func.writesPC = true;
+	func.fixedJump = false;
 	return func;
 };
 
@@ -450,6 +460,7 @@ ARMCore.prototype.compileArm = function(instruction) {
 		var rm = instruction & 0xF;
 		op = this.armCompiler.constructBX(rm, condOp);
 		op.writesPC = true;
+		op.fixedJump = false;
 	} else if (!(instruction & 0x0C000000) && (i == 0x02000000 || (instruction & 0x00000090) != 0x00000090)) {
 		var opcode = instruction & 0x01E00000;
 		var s = instruction & 0x00100000;
@@ -878,6 +889,7 @@ ARMCore.prototype.compileArm = function(instruction) {
 				op = this.armCompiler.constructB(immediate, condOp);
 			}
 			op.writesPC = true;
+			op.fixedJump = true;
 			break;
 		case 0x0C000000:
 			// Coprocessor data transfer
@@ -1001,6 +1013,7 @@ ARMCore.prototype.compileThumb = function(instruction) {
 			// BX
 			op = this.thumbCompiler.constructBX(rd, rm);
 			op.writesPC = true;
+			op.fixedJump = false;
 			break;
 		}
 	} else if ((instruction & 0xF800) == 0x1800) {
@@ -1253,6 +1266,7 @@ ARMCore.prototype.compileThumb = function(instruction) {
 				var condOp = this.conds[cond];
 				op = this.thumbCompiler.constructB1(immediate, condOp);
 				op.writesPC = true;
+				op.fixedJump = true;
 			}
 			break;
 		case 0x6000:
@@ -1269,6 +1283,7 @@ ARMCore.prototype.compileThumb = function(instruction) {
 				immediate <<= 1;
 				op = this.thumbCompiler.constructB2(immediate);
 				op.writesPC = true;
+				op.fixedJump = true;
 				break;
 			case 0x0800:
 				// BLX (ARMv5T)
@@ -1292,6 +1307,7 @@ ARMCore.prototype.compileThumb = function(instruction) {
 				// BL(2)
 				op = this.thumbCompiler.constructBL2(immediate);
 				op.writesPC = true;
+				op.fixedJump = true;
 				break;
 			}
 			break;
