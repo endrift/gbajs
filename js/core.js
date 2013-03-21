a4e5ac0b8c13c799e6eae2d13ed39fcb0f22236b
Jeffrey Pfau <jeffrey@endrift.com>
2013-03-21 00:19:20 -0700

    BL(2) is not a fixed jump when not mixed with BL(1)

--- js/core.js
+++ js/core.js
@@ -1056,6 +1056,7 @@ ARMCore.prototype.compileArm = function(instruction) {
 	}
 
 	op.execMode = this.MODE_ARM;
+	op.fixedJump = op.fixedJump || false;
 	return op;
 };
 
@@ -1455,7 +1456,7 @@ ARMCore.prototype.compileThumb = function(instruction) {
 				// BL(2)
 				op = this.thumbCompiler.constructBL2(immediate);
 				op.writesPC = true;
-				op.fixedJump = true;
+				op.fixedJump = false;
 				break;
 			}
 			break;
@@ -1467,5 +1468,6 @@ ARMCore.prototype.compileThumb = function(instruction) {
 	}
 
 	op.execMode = this.MODE_THUMB;
+	op.fixedJump = op.fixedJump || false;
 	return op;
 };
