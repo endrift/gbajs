0cf0ac7d624eaae032e19ba2f154e0ae1ce73796
Jeffrey Pfau <jeffrey@endrift.com>
2015-12-18 22:42:54 -0800

    SIGH.

--- js/audio.js
+++ js/audio.js
@@ -253,7 +253,10 @@ GameBoyAdvanceAudio.prototype.writeEnable = function(value) {
 		if (value) {
 			this.jsAudio.connect(this.context.destination);
 		} else {
-			this.jsAudio.disconnect(this.context.destination);
+			try {
+				this.jsAudio.disconnect(this.context.destination);
+			} catch (e) {
+			}
 		}
 	}
 };
